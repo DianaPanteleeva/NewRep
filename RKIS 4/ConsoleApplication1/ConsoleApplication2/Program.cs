181f2ec9f4fce1e47328ac3a11087026277cfc51
dianapanteleeva <dianapanteleeva84686@gmail.com>
2023-12-16 10:53:16 +0700

    add sln

---
+++ RKIS 4/ConsoleApplication1/ConsoleApplication2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string path = @"C:\Users\public.COPP\Desktop\numsTask2.txt";
+        StreamReader reader = new StreamReader(path);
+        string[] nums = File.ReadAllText(path).Split(';');
+        double sum = 0;
+        double a = 0;
+        foreach (string num in nums)
+        {
+            double.TryParse(num, out a);
+            if (a <= 0)
+            {
+                break;
+            }
+
+            sum += a;
+        }
+        Console.WriteLine(sum);
+    }
+}
---
+++ RKIS 4/ConsoleApplication1/ConsoleApplication4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System; 
+using System.IO;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        string path = @"C:\Users\public.COPP\Desktop\Task4.txt";
+        StreamReader reader = new StreamReader(path);
+        string[] numbers = File.ReadAllText(path).Split(' ');
+        int count = 0;
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            if (numbers[i] == numbers[i - 1])
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine(count);
+
+    }
+}
