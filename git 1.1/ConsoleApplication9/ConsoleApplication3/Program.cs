42239edc9063de25470b329977098a6305c72b83
dianapanteleeva <dianapanteleeva84686@gmail.com>
2023-12-16 10:05:26 +0700

    first commit

---
+++ git 1.1/ConsoleApplication9/ConsoleApplication3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите начало диапазона:");
+        int start = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите конец диапазона:");
+        int end = int.Parse(Console.ReadLine());
+        int[] array = RandomArray(start, end);
+        foreach (int number in array)
+        {
+            Console.Write(number + " ");
+        }
+    }
+
+    static int[] RandomArray(int start, int end)
+    {
+        Random rnd = new Random();
+        int[] array = new int[10];
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = rnd.Next(start, end +1);
+        }
+        return array;
+    }
+}
---
+++ git 1.1/ConsoleApplication9/ConsoleApplication9/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApplication9
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+            Random rnd = new Random();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(1, 100);
+                Console.WriteLine(numbers[i]);
+            }
+            int minIndex = 0;
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] < numbers[minIndex])
+                {
+                    minIndex = i;
+                }
+            } 
+            Console.WriteLine($"Номер минимального элемента:{minIndex}");
+
+        }
+    }
+}
