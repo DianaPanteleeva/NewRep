e7ac6e71bd097a2eacb43a1ca2e5d9531518e572
DianaPanteleeva <153964992+DianaPanteleeva@users.noreply.github.com>
2023-12-19 11:22:21 +0700

    Add files via upload

---
+++ RKIS6/задание 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApplication2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = @"C:\Users\Pante\Desktop\numsTask2.txt";
+            StreamReader reader = new StreamReader(path);
+            string line = "";
+            while (!reader.EndOfStream)//достигнут ли конец файла
+            {
+                string word = reader.ReadLine();
+                line += word + " ";
+            }
+            
+            Console.WriteLine(line);
+            
+            reader.Close();
+        }
+    }
+}
