5acf9623b218f0477c1dd46249d5ad62cbed4836
DianaPanteleeva <153964992+DianaPanteleeva@users.noreply.github.com>
2023-12-19 08:45:44 +0700

    Add files via upload

---
+++ RKIS5/ConsoleApplication1/ConsoleApplication2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;   
+using System.IO;  
+using System.Linq;  
+
+namespace ConsoleApplication2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("введите кол-во строк");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("введите кол-во столбцов");
+            int m = int.Parse(Console.ReadLine());
+            int[,] a = new int[n, m];
+            Console.WriteLine("введите элементы матрицы");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    a[i, j] = int.Parse(Console.ReadLine());
+                }
+                Console.Write(a[n,m]);
+            }
+
+            int[,] newA = new int[n, m + 1];//создаем новую матрицу
+            for (int i = 0; i < n; i++)//копируем элементы из 1ой матрицы
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    newA[i, j] = a[i, j];
+                }
+            }
+
+            for (int i = 0; i < n; i++)//добавляем новый столбец с чет.кол-вом единиц
+            {
+                int count = 0;
+                for (int j = 0; j < m; j++)
+                {
+                    if (a[i, j] == 1)
+                    {
+                        count++;
+                    }
+                }
+
+                newA[i, m] = count % 2 == 0 ? 0 : 1;
+            }
+            Console.WriteLine("новая матрица:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m+1; j++)
+                {
+                    Console.Write(newA[i,j]+ " "); 
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ RKIS5/ConsoleApplication1/ConsoleApplication5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;  
+using System.IO; 
+using System.Linq; 
+
+class MainClass {  
+    public static void Main (string[] args)  
+    {  
+        string path = @"C:\Users\public.COPP\Desktop\numsTask5.txt";  
+        StreamReader reader = new StreamReader(path);
+        string[] numbers = File.ReadAllText(path).Split(' ');
+        int[] number = new int[numbers.Length];
+       
+        for (int i = 0; i < numbers.Length; i++) 
+        { 
+            number[i] = int.Parse(numbers[i]); //преобразуем строки целых чисел в формат int 
+        }
+        int min = number.Min();
+        int max = number.Max();
+        
+        int sum = 0;
+        int count = 0;
+        int startIndex = Array.IndexOf(number, min) + 1; // Индекс следующего элемента после минимального
+        int endIndex = Array.IndexOf(number, max); // Индекс максимального элемента
+
+        for (int i = startIndex; i < endIndex; i++)
+        {
+            sum += number[i];
+            count++;
+        }
+        
+        double average = (double)sum / count;
+        
+        Console.WriteLine($"Среднее арифметическое элементов между минимальным и максимальным: {average}");
+    }
+}
