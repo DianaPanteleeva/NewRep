9587faba75a552bb6db7b5e90c8fb2ab8e072fe0
DianaPanteleeva <153964992+DianaPanteleeva@users.noreply.github.com>
2023-12-22 20:59:11 +0700

    Add files via upload

---
+++ RKIS 4/задание 1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int product = 1;
+        for (int i = 3; i <= n; i += 3)
+        {
+            product *= i;
+            
+        }
+        Console.WriteLine(product);
+
+    }
+}
---
+++ RKIS 4/задание 6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System; 
+
+class MainClass { 
+    public static void Main (string[] args)
+    {
+        Console.Write("Введите значение координаты a = ");
+        float a = float.Parse(Console.ReadLine());
+        Console.Write("Введите значение координаты b = ");
+        float b = float.Parse(Console.ReadLine());
+
+        if ((b >= -3) && (b <= (2 * a) + 2) && (b <= (-2 * a) + 2))
+        {
+            Console.WriteLine("Точка принадлежит заштрихованной области");
+        }
+        else
+        {
+            Console.WriteLine("Точка не приндалежит заштрихованной области");
+        }
+        Console.ReadLine();
+    }
+}
