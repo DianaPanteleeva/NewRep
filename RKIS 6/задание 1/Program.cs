089031b13c795ca9f94e50f161144ded14aebb38
DianaPanteleeva <153964992+DianaPanteleeva@users.noreply.github.com>
2023-12-22 21:00:46 +0700

    Add files via upload

---
+++ RKIS 6/задание 1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;  
+using System.IO;
+
+class MainClass {  
+    public static void Main (string[] args)  
+    {  
+        string path = @"C:\Users\Pante\Desktop\numsTask1.txt";  
+        StreamReader reader = new StreamReader(path);
+        string line = reader.ReadLine();
+        
+        string word = "";
+        
+        for (int i = 0; i < line.Length; i++)
+        {
+            if (line[i] != ' ')
+            {
+                word += line[i];
+            }
+            else
+            {
+                if (word.Length % 2 != 0)
+                {
+                    Console.WriteLine(word);
+                }
+
+                word = "";
+            }
+        }
+        if (word.Length % 2 != 0)
+        {
+            Console.WriteLine(word);
+        }
+        reader.Close();
+    }
+}
---
+++ RKIS 6/задание 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApplication2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = @"C:\Users\Pante\Desktop\numsTask2.txt";
+            StreamReader reader = new StreamReader(path);
+            string line = "";
+            
+            while (!reader.EndOfStream)//достигнут ли конец файла
+            {
+                string word = reader.ReadLine();
+                line += word + " "; //считывается каждая строка из файла и добавляется к переменной line
+            }
+            
+            Console.WriteLine(line);
+            reader.Close();
+        }
+    }
+}
