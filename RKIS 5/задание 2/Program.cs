32c1916efce1c20f7423f6bdd831967ed7e90f2d
DianaPanteleeva <153964992+DianaPanteleeva@users.noreply.github.com>
2023-12-22 20:59:57 +0700

    Add files via upload

---
+++ RKIS 5/задание 2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;    
+using System.IO;
+
+class MainClass {    
+    public static void Main (string[] args)    
+    {    
+        string path = @"C:\Users\Pante\Desktop\numsTask2.txt";    
+        StreamReader reader = new StreamReader(path); 
+        string lines = reader.ReadLine(); 
+ 
+        string[] numStrings = lines.Split(';'); 
+        double[] numbers = new double[numStrings.Length]; 
+        for (int i = 0; i < numbers.Length; i++) 
+        { 
+            numbers[i] = double.Parse(numStrings[i]); //преобр. в вещественное число
+        } 
+        reader.Close(); 
+        
+        for (int i = 0; i < numbers.Length - 1; i++)//перебираем каждый элемент массива кроме последнего 
+        { 
+            int minIndex = i; 
+            for (int j = i + 1; j < numbers.Length; j++) 
+            { 
+                if (numbers[j] < numbers[minIndex]) //находим инд. наименьшего числа 
+                { 
+                    minIndex = j; 
+                } 
+            } 
+            double temp = numbers[i];//меняем текущий элемент с элементом с мин.знач 
+            numbers[i] = numbers[minIndex]; 
+            numbers[minIndex] = temp; 
+        } 
+ 
+        StreamWriter writer = new StreamWriter(path); 
+        foreach (double num in numbers) 
+        { 
+            writer.Write(num + ";"); 
+        } 
+        writer.Close(); 
+    }   
+}
---
+++ RKIS 5/задание 3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System; 
+using System.IO;
+using System.Linq;
+
+class MainClass { 
+    public static void Main (string[] args) 
+    { 
+       
+        string path = @"C:\Users\public.COPP\Desktop\numsTask3.txt";  
+        StreamReader reader = new StreamReader(path); 
+
+        string[] numbers = File.ReadAllText(path).Split(' ');
+        int[] intNumbers = new int[numbers.Length];//массив для хранения чисел в формате int
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            intNumbers[i] = int.Parse(numbers[i]); //преобразуем строки целых чисел в формат int
+        }
+
+        int minIndex = Array.IndexOf(intNumbers, intNumbers.Min());
+        if (minIndex > 0)
+        {
+            int sum = 0;
+            for (int i = 0; i < minIndex; i++)
+            {
+                sum += intNumbers[i];
+            }
+
+            double a = (double)sum / minIndex;
+            Console.WriteLine($"среднее арифметическое элементов до минимального = {a}");
+        }
+        else
+        {
+            Console.WriteLine("невозможно найти ср.арифметическое(элемент находится в начале массива)");
+        }
+    }
+}
